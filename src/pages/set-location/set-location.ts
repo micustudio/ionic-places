11d3342eaae08b22ec851b6469f6ae75a5bdf326
Michael <micumieu@gmail.com>
2017-09-24 23:58:00 -0700

    Add google maps api.

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Location } from '../../models/location';
+import { NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SetLocationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-set-location',
   templateUrl: 'set-location.html',
 })
 export class SetLocationPage {
+  location: Location
+  marker: Location;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(private navParams: NavParams) {
+    this.location = this.navParams.get('location');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SetLocationPage');
+  onSetMarker(event: any) {
+    console.log(event);
+    this.marker = new Location(event.coords.lat, event.coords.lng);
   }
 
 }
